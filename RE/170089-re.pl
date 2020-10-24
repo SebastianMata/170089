242ac71a5007b828858dc2bdac57af98af8fa40b
Gabriel Loyola <43822079+GaboLoyola@users.noreply.github.com>
2020-10-23 20:13:08 -0500

    Create 170089-re.pl

---
+++ RE/170089-re.pl
@@ -0,0 +1,16 @@
+# ********************************************************************************************* #
+#                                                                                               #
+#                                 Major:    IT Engineering                                      #
+#       Name of the specialty professor:    González Ibarra Juan Carlos                         #
+#                               Subject:    Theory of Computation                               #
+#               Name of the institution:    Universidad Politecnica de San Luis Potosí          #
+#                     Name & enrollment:    Baltazar Loyola Gabriel Enrique, 170089             #
+#                                                                                               #
+#                           Description:    Perl program for the evaluation of a string         #
+#                                           using regular expressions and simulating            #
+#                                           a compiler.                                         #
+#                                                                                               #
+#       Written:       22/10/2020                                                               #
+#       Last updated:  23/10/2020                                                               #
+#                                                                                               #
+# ********************************************************************************************* #
